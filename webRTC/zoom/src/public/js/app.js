f0b43eb8200bd2e02f10db443f28250c0c7ce4db
heisje <heeje1996@gmail.com>
2023-01-06 17:45:46 +0900

    노마드코더 2챕터

--- webRTC/zoom/src/public/js/app.js
+++ webRTC/zoom/src/public/js/app.js
@@ -1,1 +1,15 @@
-alert("")
+const socket = io();
+
+const welcome = document.getElementById("welcome")
+const form = welcome.querySelector("form");
+
+function handleListen(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", { payload: input.value }, ()=>{
+        console.log("server is done!");
+    })
+    input.value = ""
+}
+
+form.addEventListener("submit", handleListen)
--- webRTC/zoom/src/server.js
+++ webRTC/zoom/src/server.js
@@ -1,3 +1,5 @@
+import http from "http"
+import { Server } from "socket.io";
 import express from "express";
 
 const app = express();
@@ -8,5 +10,15 @@ app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => res.render("home"));
 app.get("*", (req, res) => res.redirect("/"));
 
+
+const httpServer = http.createServer(app)
+// http://localhost:3000/socket.io/socket.io.js
+const wsServer = new Server(httpServer);
+
+wsServer.on("connection", (socket) => {
+    socket.on("enter_room", (msg, done) => console.log(msg));
+})
+
+
 const handleListen = () => console.log('Listening on http://localhost:3000')
-app.listen(3000, handleListen);
+httpServer.listen(3000, handleListen)
